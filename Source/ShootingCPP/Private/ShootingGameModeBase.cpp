552551720390f82eeff00d691905363645cd27cd
J0Min <rudals489@gmail.com>
2026-05-08 16:15:05 +0900

    feat : MenuWidget 클래스 생성 (UserWidget 상속)

--- Source/ShootingCPP/Private/ShootingGameModeBase.cpp
+++ Source/ShootingCPP/Private/ShootingGameModeBase.cpp
@@ -3,7 +3,32 @@
 
 #include "ShootingGameModeBase.h"
 
+#include "MainWidget.h"
+#include "Components/TextBlock.h"
+
 void AShootingGameModeBase::AddScore(int32 point)
 {
 	currentScore += point;
+	PrintScore();
+}
+
+void AShootingGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	//mainWidget BP파일을 메모리에 로드
+	mainUI = CreateWidget<UMainWidget>(GetWorld(),mainWidget);
+	//위젯이 메모리에 로드되면 뷰포트에 출력
+	if (mainUI != nullptr)
+	{
+		mainUI->AddToViewport();
+	}
+}
+
+void AShootingGameModeBase::PrintScore()
+{
+	if (mainUI != nullptr)
+	{
+		mainUI->scoreData->SetText(FText::AsNumber(currentScore));
+	}
 }
